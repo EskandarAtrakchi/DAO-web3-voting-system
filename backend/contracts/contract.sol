b715c4572d18861274bc9bbf9fb4e565552e1609
EskandarAtrakchi <eskandaratrakchi8@gmail.com>
2025-02-27 11:51:34 +0000

    hardhad testing

--- backend/contracts/contract.sol
+++ backend/contracts/contract.sol
@@ -12,39 +12,37 @@ contract DAO {
 
     mapping(uint => Proposal) public proposals;
     uint public proposalCount;
-    
+
     event ProposalCreated(uint proposalId, string description);
     event Voted(uint proposalId, address voter, bool vote);
-    
+
     function createProposal(string memory _description) public {
         proposalCount++;
-        proposals[proposalCount] = Proposal({
-            description: _description,
-            yesVotes: 0,
-            noVotes: 0,
-            exists: true
-        });
+        proposals[proposalCount].exists = true;
+        proposals[proposalCount].description = _description;
+        proposals[proposalCount].yesVotes = 0;
+        proposals[proposalCount].noVotes = 0;
+
         emit ProposalCreated(proposalCount, _description);
     }
-    
-    function vote(uint _proposalId, bool _vote) public {
+
+    function vote(uint256 _proposalId, bool _vote) public {
         require(proposals[_proposalId].exists, "Proposal does not exist");
         require(!proposals[_proposalId].hasVoted[msg.sender], "Already voted");
-        
-        proposals[_proposalId].hasVoted[msg.sender] = true;
-        
+
         if (_vote) {
             proposals[_proposalId].yesVotes++;
         } else {
             proposals[_proposalId].noVotes++;
         }
-        
+
+        proposals[_proposalId].hasVoted[msg.sender] = true; // Mark voter as voted
+
         emit Voted(_proposalId, msg.sender, _vote);
     }
-    
+
     function getProposal(uint _proposalId) public view returns (string memory, uint, uint) {
         require(proposals[_proposalId].exists, "Proposal does not exist");
-        Proposal storage p = proposals[_proposalId];
-        return (p.description, p.yesVotes, p.noVotes);
+        return (proposals[_proposalId].description, proposals[_proposalId].yesVotes, proposals[_proposalId].noVotes);
     }
 }
