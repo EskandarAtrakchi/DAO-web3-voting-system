8d671cee1315b0da4727a1f501050066f59759ed
EskandarAtrakchi <eskandaratrakchi8@gmail.com>
2025-03-19 15:47:54 +0000

    code updated

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      config.externals.push('pino-pretty', 'lokijs', 'encoding');
-      return config;
-    },
-    i18n: {
-      locales: ['en-US', 'zh-CN'],
-      defaultLocale: 'en-US',
-    },
-  };
+  output: 'export',
+  assetPrefix: isProd ? '/DAO-web3-voting-system/' : '',
+  basePath: isProd ? '/DAO-web3-voting-system' : '',
+};
+
+module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     webpack: (config) => {
+//       config.externals.push('pino-pretty', 'lokijs', 'encoding');
+//       return config;
+//     },
+//     i18n: {
+//       locales: ['en-US', 'zh-CN'],
+//       defaultLocale: 'en-US',
+//     },
+//   };
   
-  module.exports = nextConfig;
+//   module.exports = nextConfig;
   
